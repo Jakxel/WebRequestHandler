9f6745a4fbb057a3567115a6c553990e9d551afc
Jakxel Carreon <147444918+Jakxel@users.noreply.github.com>
2026-02-16 03:42:20 +0000

    Dinamic HTML
    Jakxel ➜ /workspaces/WebRequestHandler (main) $ curl http://localhost:8000/proyecto/web-uno?autor=luis~
    <h1>Proyecto: web-uno Autor: luis~</h1>@Jakxel ➜ /workspaces/WebRequestHandler (main) $

--- web.py
+++ web.py
@@ -10,19 +10,26 @@ class WebRequestHandler(BaseHTTPRequestHandler):
         return dict(parse_qsl(self.url().query))
 
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/html")
-        self.end_headers()
-        self.wfile.write(self.get_response().encode("utf-8"))
+        if self.valida_autor():
+            self.send_response(200)
+            self.send_header("Content-Type", "text/html")
+            self.end_headers()
+            self.wfile.write(self.get_html().encode("utf-8"))
+        else:
+            self.send_error(404, "The author doesnt exist")
 
-    def get_response(self):
-        return f"""
-    <h1> Hola Web </h1>
-    <p> URL Parse Result : {self.url()}         </p>
-    <p> Path Original: {self.path}         </p>
-    <p> Headers: {self.headers}      </p>
-    <p> Query: {self.query_data()}   </p>
-"""
+    def valida_autor(self):
+        return 'autor' in self.query_data()
+        
+    def get_html(self):
+        path_parts = self.url().path.strip("/").split("/")
+
+        if len(path_parts) >= 2:
+            proyecto = path_parts[1]
+            autor = self.query_data()['autor']
+            return f"<h1>Proyecto: {proyecto} Autor: {autor}</h1>"
+        else:
+            return "<h1>Invalid route</h1>"
 
 
 if __name__ == "__main__":
